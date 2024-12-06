ac5ce4caa71959d6dd620261d2e277be236aa140
erhn05 <73742853+erhn05@users.noreply.github.com>
2024-12-06 22:45:10 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let currentIndex = 0;
+
+function moveSlide(step) {
+    const images = document.querySelectorAll('.carousel-slide');
+    currentIndex += step;
+
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;
+    } else if (currentIndex >= images.length) {
+        currentIndex = 0;
+    }
+
+    const newTransformValue = -currentIndex * 100;
+    document.querySelector('.carousel').style.transform = `translateX(${newTransformValue}%)`;
+}
