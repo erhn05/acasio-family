f9f8ef1b9175123b3b5646662ff680a1acbe0deb
erhn05 <73742853+erhn05@users.noreply.github.com>
2024-12-07 18:29:05 +0800

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,16 +1,144 @@
-// Fetch the JSON file and populate the carousel
-fetch('data/photos-past-christmases.json')
-.then(response => response.json())
-.then(images => {
-    const carousel = document.getElementById('carousel');
-    images.forEach(image => {
-        const item = document.createElement('div');
-        item.className = 'carousel-slide';
-        item.innerHTML = `
+// Embedded JSON data
+const images = [
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczMxsNjFp-Wif5hjH0EBjyYbeArlmzXryq5XdhugqhxnV0g5lfh2-v4=w1433-h941-s-no-gm?authuser=0",
+		"alt": "001",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczPLYP1r3xXMw65-XzVfXqIZXdwdgjez9Up9gCOJ4bSreLY0s0OsfU4=w1372-h951-s-no-gm?authuser=0",
+		"alt": "002",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczPFG9_yWET-h9SBqm4PGzoSkMLJa2DX_4BAud_RG67hlMxye4xhMjE=w1342-h951-s-no-gm?authuser=0",
+		"alt": "003",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczNm0XxhGepUR5M4cP1eO9DQju2YmVSSAIbckmBKO-pyUY5PLxDVVl0=w1427-h929-s-no-gm?authuser=0",
+		"alt": "004",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczPVIhslGL3TCxnJHC-SP5WVxTAsUcBIFi78yUql9iAaJ6CawF-eeSQ=w1376-h951-s-no-gm?authuser=0",
+		"alt": "005",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczMIiQLvMZjqGFVyg_FXnwFlxh4h5mZmaK9ZY_DX_-H5XUTJOUNln74=w1342-h951-s-no-gm?authuser=0",
+		"alt": "006",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczMt2zI5JtGFpFNu7KFrY_BGtmhbKEc6YZrydsANBeXxx031tvEMzy8=w661-h951-s-no-gm?authuser=0",
+		"alt": "007",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczPUXgDZNRXNc9cTY42O4T_HsfuW2AtYGCM5kI_VdiREc8M7knL8MyQ=w960-h720-s-no-gm?authuser=0",
+		"alt": "008",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczMFpZ-EGZHIB8Mtr0cuACI0Ezw4cHjvfjIc7MFvU6HIzCvE2ebza2U=w960-h720-s-no-gm?authuser=0",
+		"alt": "009",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczPLC0fFH55zuylH3SVMPCriG1S56QzDC5C471Ql3NvMngEC5ltlH38=w960-h720-s-no-gm?authuser=0",
+		"alt": "010",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczO3vSYFiB9lgzE4qEhja7nSCv0gyDzk7RTN451Q8kmPOLRH3EEbZUY=w960-h643-s-no-gm?authuser=0",
+		"alt": "011",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczM_z6oyDVeI5TbSTLIZDsrJfAKM99MGkUtIuNF7uYRjSaqZn6_7N2M=w960-h720-s-no-gm?authuser=0",
+		"alt": "012",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczOcpn-JUIbADqDEOl9VwRlpc6Oqxe50ULx_29Xf9oyRmGbWIAqajSM=w960-h643-s-no-gm?authuser=0",
+		"alt": "013",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczP6HF3cH39ZAYdCUdcaSDHZjMhvoKY1RYVS2mKW0MMO_iIuFKH2Fi4=w960-h720-s-no-gm?authuser=0",
+		"alt": "014",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczMUOCUpUgzJYZO_OYKue5NDZLwBMbyQKeDtHT1JUr4eC7qShKW4a2I=w960-h643-s-no-gm?authuser=0",
+		"alt": "015",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczM7yBbdKEyZgy8bavTtWbV0pInz5p2-DzoY9gq5ybXtt9DdB50TutI=w637-h951-s-no-gm?authuser=0",
+		"alt": "016",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczOtxubgMy8wDzAdyXvX1HSsM_kQU5THGC8JUW6eAc7zk6M9lAhehOg=w960-h643-s-no-gm?authuser=0",
+		"alt": "017",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczNWBek-YpCpiFVJvCOx5HQ4wEuHpUd-rL27nycqcqZuRU_IDaxkzig=w960-h720-s-no-gm?authuser=0",
+		"alt": "018",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczOG5jpzeKDSxvPeIbg0GCkGAoS8mgxXBu-V1f36IzKjdReM6lMNnhk=w960-h720-s-no-gm?authuser=0",
+		"alt": "019",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczPzU5GSNKy3i3EqVPqPy3hnSY9fUPOY8LrOxI6rFoH8toAhLANxTiY=w960-h653-s-no-gm?authuser=0",
+		"alt": "020",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczMUBrlMyU5DE59R5tb7qua-HcMmDPMsxTdp7PjITyCgC4nRi_zPRoU=w960-h845-s-no-gm?authuser=0",
+		"alt": "021",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczNUHAKC-66Q9RWPtHJsQqiKv1RNHUmQ4iJtCNJiaEXkj9XVC4CPH3I=w713-h951-s-no-gm?authuser=0",
+		"alt": "022",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczOtbgr-WQIxwDKD4WZ6UdjIIXdh8BQnhuaVOrQWk74DLK3pT8mq2UU=w713-h951-s-no-gm?authuser=0",
+		"alt": "023",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczNeMNFM33x1AoUQSjM8R81dCoFWN8rbsRdBo7GCZe-Cod-gdjhEDKQ=w713-h951-s-no-gm?authuser=0",
+		"alt": "024",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczN3fqX-6QnOa_a0pZmSLCz1lrqm1Xy-4nmAa4IpaYLnQUt44U1xLxs=w713-h951-s-no-gm?authuser=0",
+		"alt": "025",
+		"caption": ""
+	},
+	{
+		"src": "https://photos.fife.usercontent.google.com/pw/AP1GczM1DbJzoUW7B2X_jxu_MnWoKBIPZQiao-DUG-mNDnqqk4MOFbS1qoA=w1268-h951-s-no-gm?authuser=0",
+		"alt": "026",
+		"caption": ""
+	}
+]
+
+// Populate the carousel
+images.forEach(image => {
+    const carouselSlideContent = `
+        <div class="carousel-slide">
             <img src="${image.src}" alt="${image.alt}" class="carousel-slide-image">
             <caption>${image.caption}</caption>
-            `;
-            carousel.appendChild(item);
-    });
-})
-.catch(error => console.error('Error loading images:', error));
+        </div>
+    `;
+    document.getElementById('carousel').innerHTML += carouselSlideContent;
+});
