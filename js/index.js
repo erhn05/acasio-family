9b5537d75d872b5d2d3814001ed6efdc40056a5e
erhn05 <73742853+erhn05@users.noreply.github.com>
2025-12-07 17:37:01 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,74 +1,16 @@
-// Embedded JSON data
-const pastChristmasesPhotos = [
-	{
-		"src": "https://lh3.googleusercontent.com/pw/AP1GczPxCrgSg9qvXXcuuNx41a69hJdrKw5TNyyXQDmgZbSqk6ygbl91FMU0PvOeffUSizttcTT6TbwrfvRpfGIpuSb65sNdMn3wH-3xk6kuUchm0N1nSTMyn-XGynFZfbInZHaJ-aLyjXH8uJPtYftCoDJ6=w1062-h810-s-no-gm?authuser=0",
-		"alt": "001",
-		"caption": ""
-	},
-	{
-		"src": "https://lh3.googleusercontent.com/pw/AP1GczP_BTJAJJI7QLughfTzeXA54neQpcDSIVYfSRaCF7L2tBOg2Rk7RwjHg5Ey4QP07HlQrsSyNgf6nfALdlkfLoSYjfVQpJEqQRsuqQiFwxd3r-sJnNOsWYsUrUVF9NruA6SLrhBXTYnbibnhADjW_z7m=w1433-h941-s-no-gm?authuser=0",
-		"alt": "002",
-		"caption": ""
-	},
-	{
-		"src": "https://lh3.googleusercontent.com/pw/AP1GczOp3hEExwfF03ddoJ8HmxsadEFO8PMImTmXQgehbIXAC57kRX7-2bnNJHwJFMT3YTuW98XBY2OE5hTofOkUfjsM199MyQ4NeYV72R1HmPeePr3SGo6VRquINwKqVTrfqsP63-DxcX1xvXZvpYoboIr4=w1342-h951-s-no-gm?authuser=0",
-		"alt": "003",
-		"caption": ""
-	},
-	{
-		"src": "https://lh3.googleusercontent.com/pw/AP1GczOOAwStw-c3UTX6fcaRFKYmB4ZscR7Qfu-e4N626dRVxrGufxX_cGxOqoRPpT31jtfC26L51nB1WG2oToWENLhat-IWwtiI_2d5mOmrnIVcacWLHDJnHrO9IW7KUxh7kO35JTo7znZZYj1BHmuaxX6k=w1376-h951-s-no-gm?authuser=0",
-		"alt": "004",
-		"caption": ""
-	},
-	{
-		"src": "https://lh3.googleusercontent.com/pw/AP1GczNqJ5OIo-iUZjUNOLOFnuA5OhYB9hkrPK0ovFjqTLReeXSX_fp3y29LOwsZSntrC74bEMn6Rxkk5mNWBIq_z6nJlWWO14zYAinR9I3K2lRtOETYEI2GWaa99pzhzu05AfsH4UV0OhAStUTdYb-MBZaa=w1342-h951-s-no-gm?authuser=0",
-		"alt": "005",
-		"caption": ""
-	},
-	{
-		"src": "https://lh3.googleusercontent.com/pw/AP1GczN9DVj__VB2BVeuSh4zmTWRSSUm34_z06TFwnekm9SlGzm1yd80HTdfM0gOWrRJUnvJXEj4LrPbxpAeypOiFHCul9CiukFoDJ1I6rP1cKcFL42FiDTVbFAPh2pq87BwZdKjHvGKdhMb5SQRVVudGlYD=w1427-h929-s-no-gm?authuser=0",
-		"alt": "006",
-		"caption": ""
-	},
-	{
-		"src": "https://lh3.googleusercontent.com/pw/AP1GczOKU8oZZoixizixxCbHraisuxP0CbQHQWoKPQcSb6QuhwaGfTTbpweIxw3elTYeeiP_oxbvqvuhn3ELyQupLaa4YFPUJtrCSZdQ7111Pw9jRvzzl58FRYGTYRP8_cjgjo7YwSSkl1rl1RwTSEujqyUh=w960-h643-s-no-gm?authuser=0",
-		"alt": "007",
-		"caption": ""
-	},
-	{
-		"src": "https://lh3.googleusercontent.com/pw/AP1GczO52-3XncPybXA-ybk5BKfbPg8XxOivBTmGIiO5vg9b0CRw9sQHPeM7pAeWzTuru9msMrDQOqtF81LBroCJ8q7-OA0s16QY2gM82qxh-7Tp9slsZRT3YfCW8f-gYHBRQWk_MJEsRfxaZfROUdNMS30Y=w1268-h951-s-no-gm?authuser=0",
-		"alt": "008",
-		"caption": ""
-	},
-	{
-		"src": "https://lh3.googleusercontent.com/pw/AP1GczNccSpdO_DwT7bZNy3eS5IFfOGUrddaIjdn2TjfJsuDMTsmxEgZ4HnOESfNRb42wlzDNQImqoVaWDkU2gCd-zKSkh5z3s0HiU-iRBKw73pzjFEiNO4wqJBkuhTsjJpcOAlcJlOCWd1BXr0AlB5lqmLy=w960-h720-s-no-gm?authuser=0",
-		"alt": "009",
-		"caption": ""
-	},
-	{
-		"src": "https://lh3.googleusercontent.com/pw/AP1GczOZgYc6XVSSCzxerJ-1jVfupc34tLm6AJBSaAPxnBLzIUTkgMiM-Cezq2-ef2RBvLW8SeNJJAKAbN172eQW17ZQvdRPfFIyD_gFO65ZomZFToSz9Ul3yz483a8uFHmqb8qMgGqwpLmd72qF0_VfEirN=w713-h951-s-no-gm?authuser=0",
-		"alt": "010",
-		"caption": ""
-	},
-	{
-		"src": "https://lh3.googleusercontent.com/pw/AP1GczN9RlUdemXWfHjlDlgpXQX8sb7aYMVx0vwtqAY-eHF-9GeUc4Je3KNLEF6ncmDjlXYaXlk6CETY28-F71KfmcAhMJVPUmlLC45BpTPmDp7n-JbJPG-_rrxefyW-SF88tCLqtybGrKyT64W9lDIc5Qp7=w1268-h951-s-no-gm?authuser=0",
-		"alt": "011",
-		"caption": ""
-	},
-	{
-		"src": "https://lh3.googleusercontent.com/pw/AP1GczMJL8MUX8pyfdjPKXegfL7Gc2yoXLI5ZQOCvrmxwF0zbTNdSlFQxA_5Oy9LOxuSUNXRbSf5KrPeg1EkwpIlu7W3KI_Blss4imW6UZzOiV9ZTvna92EAsZ7mZf_TyQsoCPJ5G7CdMfYPEXbVgfUJ3M_C=w1268-h951-s-no-gm?authuser=0",
-		"alt": "012",
-		"caption": ""
-	},
-]
+const carouselEl = document.getElementById('carousel');
+carouselEl.innerHTML = '';
 
-// Populate the carousel
-pastChristmasesPhotos.forEach(photo => {
-    const carouselSlideContent = `
-        <div class="carousel-slide">
-            <img src="${photo.src}" alt="${photo.alt}" class="carousel-slide-image">
-            <caption>${photo.caption}</caption>
-        </div>
-    `;
-    document.getElementById('carousel').innerHTML += carouselSlideContent;
+// christmasPhotos comes from christmas-photos.js
+christmasPhotos.forEach(photo => {
+  const slide = document.createElement('div');
+  slide.className = 'carousel-slide';
+
+  const img = document.createElement('img');
+  img.src = photo.dataUrl;
+  img.alt = photo.alt || '';
+  img.className = 'carousel-slide-image';
+
+  slide.appendChild(img);
+  carouselEl.appendChild(slide);
 });
